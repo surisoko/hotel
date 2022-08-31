492e5b8dc9ffd4140449d91cdfa921d441cbf853
carles.suriol <carles.suriol@mediquo.com>
2022-08-31 16:49:22 +0200

    Refactor EventServiceProvider.php

--- config/events.php
+++ config/events.php
@@ -2,10 +2,8 @@
 
 return [
     'topic' => env('AWS_PUBLISHER_TOPIC'),
-
-    'region' => env('AWS_PUBLISHER_REGION', 'us-east-1'),
+    'region' => env('AWS_PUBLISHER_REGION'),
     'version' => env('AWS_PUBLISHER_VERSION', 'latest'),
-
     'credentials' => [
         'key' => env('AWS_PUBLISHER_KEY'),
         'secret' => env('AWS_PUBLISHER_SECRET'),
